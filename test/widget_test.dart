5da88798160a8b8eabe243cdb9c70ce406566b62
bmb <dard.cool@gmail.com>
2019-10-05 22:42:05 +0330

    fixed test error...

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,6 @@ void main() {
   testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-    expect(find.text('Hello World!'), findsOneWidget);
+    expect(find.text('Salam'), findsOneWidget);
   });
 }
